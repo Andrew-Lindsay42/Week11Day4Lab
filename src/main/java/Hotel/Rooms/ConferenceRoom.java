c48e8aae792e7835257a5b343b583fbc7bfb2b5c
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-10 10:57:19 +0000

    Created Ballroom Class and tests

--- src/main/java/Hotel/Rooms/ConferenceRoom.java
+++ src/main/java/Hotel/Rooms/ConferenceRoom.java
@@ -1,4 +1,14 @@
 package Hotel.Rooms;
 
-public class ConferenceRoom {
+public class ConferenceRoom extends Room {
+    private String name;
+
+    public ConferenceRoom(String name, int capacity) {
+        super(capacity);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/Rooms/ConferenceRoomTest.java
+++ src/test/java/Rooms/ConferenceRoomTest.java
@@ -1,4 +1,31 @@
 package Rooms;
 
+import Hotel.Rooms.ConferenceRoom;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+    ConferenceRoom conferenceRoom;
+
+    @Before
+    public void before(){
+        conferenceRoom = new ConferenceRoom("Ballroom", 50);
+    }
+    
+    @Test
+    public void hasName(){
+        assertEquals("Ballroom", conferenceRoom.getName());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(50, conferenceRoom.getCapacity());
+    }
+
+    @Test
+    public void startsEmpty(){
+        assertEquals(0, conferenceRoom.countGuests());
+    }
 }
